77c780a5cdae7174a1d4b2269dd09adb2b120d6d
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-18 20:19:29 +0530

    datetime 0.1

--- lib/screens/other_plan.dart
+++ lib/screens/other_plan.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:play_plan/custom_widgets/inputbox.dart';
+import 'package:play_plan/custom_widgets/button.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class OtherPlan extends StatefulWidget {
   OtherPlan({Key key}) : super(key: key);
@@ -24,7 +26,22 @@ class _OtherPlanState extends State<OtherPlan> {
                   hintText: 'Specify Plan',
                   labelText: 'Plan Name',
                 ),
-
+                Button(
+                  text: 'PICK DATE AND TIME',
+                  onPressed: () {
+                    DatePicker.showDatePicker(
+                      context,
+                      showTitleActions: true,
+                      minTime: DateTime.now(),
+                      maxTime: DateTime(2030, 12, 12),
+                      onConfirm: (date) {
+                        print('confirm $date');
+                      },
+                      currentTime: DateTime.now(),
+                      locale: LocaleType.en,
+                    );
+                  },
+                ),
               ],
             ),
           ],
