bd5ec2e6850fbfbc7dad114dd950f54edb7b5cd7
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-26 23:24:46 +0530

    aras |

--- lib/screens/user_home.dart
+++ lib/screens/user_home.dart
@@ -18,51 +18,65 @@ class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('We Plan'),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-        ),
-        body: ListView(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                      'https://cdn8.dissolve.com/p/D1028_97_086/D1028_97_086_1200.jpg'),
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.2), BlendMode.darken),
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  BigBox(
-                    text: 'PLAN',
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => PlanPage()));
-                    },
-                    padding: EdgeInsets.only(
-                        top: 28.0, left: 28.0, right: 28.0, bottom: 14.0),
-                  ),
-                  BigBox(
-                    text: 'VIEW PLANS',
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ViewPlanPage()));
-                    },
-                    padding: EdgeInsets.only(
-                        top: 14.0, left: 28.0, right: 28.0, bottom: 28.0),
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              ListView(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://cdn8.dissolve.com/p/D1028_97_086/D1028_97_086_1200.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.2), BlendMode.darken),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        BigBox(
+                          text: 'PLAN',
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PlanPage()));
+                          },
+                          padding: EdgeInsets.only(
+                              top: 28.0, left: 28.0, right: 28.0, bottom: 14.0),
+                        ),
+                        BigBox(
+                          text: 'VIEW PLANS',
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ViewPlanPage()));
+                          },
+                          padding: EdgeInsets.only(
+                              top: 14.0, left: 28.0, right: 28.0, bottom: 28.0),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              Icon(Icons.directions_transit),
+            ],
+          ),
         ),
       ),
     );
