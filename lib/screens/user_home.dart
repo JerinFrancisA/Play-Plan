179e460388e69346b96e4aa58b945935335d81c5
JerinFrancisA <jerinfrancis77@gmail.com>
2019-10-27 23:30:46 +0530

    madatory daily commit

--- lib/screens/user_home.dart
+++ lib/screens/user_home.dart
@@ -38,48 +38,6 @@ class _UserHomeState extends State<UserHome> {
           ),
           body: TabBarView(
             children: [
-              // ListView(
-              //   children: [
-              //     Container(
-              //       decoration: BoxDecoration(
-              //         image: DecorationImage(
-              //           image: NetworkImage(
-              //               'https://cdn8.dissolve.com/p/D1028_97_086/D1028_97_086_1200.jpg'),
-              //           fit: BoxFit.cover,
-              //           colorFilter: ColorFilter.mode(
-              //               Colors.black.withOpacity(0.2), BlendMode.darken),
-              //         ),
-              //       ),
-              //       child: Column(
-              //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //         children: <Widget>[
-              //           BigBox(
-              //             text: 'PLAN',
-              //             onTap: () {
-              //               Navigator.push(
-              //                   context,
-              //                   MaterialPageRoute(
-              //                       builder: (context) => PlanPage()));
-              //             },
-              //             padding: EdgeInsets.only(
-              //                 top: 28.0, left: 28.0, right: 28.0, bottom: 14.0),
-              //           ),
-              //           BigBox(
-              //             text: 'VIEW PLANS',
-              //             onTap: () {
-              //               Navigator.push(
-              //                   context,
-              //                   MaterialPageRoute(
-              //                       builder: (context) => ViewPlanPage()));
-              //             },
-              //             padding: EdgeInsets.only(
-              //                 top: 14.0, left: 28.0, right: 28.0, bottom: 28.0),
-              //           ),
-              //         ],
-              //       ),
-              //     ),
-              //   ],
-              // ),
               PlanPage(),
               ViewPlanPage(),
             ],
